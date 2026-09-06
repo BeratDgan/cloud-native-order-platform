3fe54f2c2793292743b14ddbc0b402b0aacb4a40
BeratDgan <dgan.berat@gmail.com>
2026-09-06 17:12:06 +0300

    feat: add resource group variable and configuration for Azure

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,7 @@ variable "subscription_id" {
   description = "Azure kaynaklarının oluşturulacağı subscription ID"
   type        = string
 }
+variable "resource_group_name" {
+  description = "Mevcut Azure resource group adı"
+  type        = string
+}
